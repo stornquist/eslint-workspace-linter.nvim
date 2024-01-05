384c003f62a6faeeb4f1a0a937dc90c7cf2fa448
Sebastian Törnquist <sebb.tornquist@gmail.com>
2024-01-05 15:28:36 +0100

    feat: plugin options

--- lua/lint.lua
+++ lua/lint.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+---@alias ListOptions "quickfix"|"trouble"
+
+---@class PluginOptions
+---@field auto_open? boolean
+---@field list? ListOptions
+
+---@type PluginOptions
+local default_options = {
+	auto_open = true,
+	list = "quickfix",
+}
+
+---@param opts PluginOptions
 M.setup = function(opts)
 	local status, lspconfig = pcall(require, "lspconfig")
 	if not status then
@@ -9,6 +22,8 @@ M.setup = function(opts)
 	if not lspconfig["eslint"] then
 		print("make sure eslint is installed and setup before loading eslint-workspace-lint")
 	else
+		local options = vim.tbl_deep_extend("force", default_options, opts or {})
+
 		lspconfig["eslint"].setup({
 			on_attach = function(client, bufnr)
 				local original = lspconfig["eslint"].manager.config.capabilities.on_attach
@@ -16,14 +31,37 @@ M.setup = function(opts)
 					local workspace =
 						vim.lsp.get_active_clients({ bufnr = bufnr, name = "eslint" })[1].config.settings.workspaceFolder.uri
 					vim.api.nvim_command(
-						":set makeprg=npx\\ eslint\\ -f\\ unix\\ '" .. workspace .. "/src/**/*.{js,ts,jsx,tsx}'"
+						":set errorformat=%f:%l:%c:%m | setlocal makeprg=npx\\ eslint\\ -f\\ unix\\ '"
+							.. workspace
+							.. "/src/**/*.{js,ts,jsx,tsx}'"
 					)
 					vim.api.nvim_command(":make")
-					local len = vim.api.nvim_exec2("echo len(getqflist())", { output = true }).output
-					if tonumber(len) > 0 then
-						vim.api.nvim_command(":copen")
-					else
+
+					local quickfixEntries = vim.fn.getqflist()
+					if #quickfixEntries == 0 then
 						print("No linting errors found")
+						return
+					end
+
+					if not options.auto_open then
+						return
+					end
+
+					-- clean up quickfix list and remove invalid entries
+					for i = #quickfixEntries, 1, -1 do
+						if quickfixEntries[i].valid == 0 then
+							table.remove(quickfixEntries, i)
+						end
+					end
+
+					vim.fn.setqflist(quickfixEntries)
+
+					if options.list == "quickfix" then
+						vim.api.nvim_command(":copen")
+					end
+
+					if options.list == "trouble" and require("trouble") then
+						require("trouble").open({ mode = "quickfix" })
 					end
 				end, { nargs = 0 })
 				original(client, bufnr)
